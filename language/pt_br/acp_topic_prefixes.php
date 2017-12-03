430380b3a7208aa8a340ea8aef12b49f1ea85674
phpBBTraducoes <eunaumtenhoid@outlook.com>
2017-12-03 15:25:39 -0200

    Traducao ADD

---
+++ language/pt_br/acp_topic_prefixes.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ *
+ * Topic Prefixes extension for the phpBB Forum Software package.
+ * Brazilian Portuguese translation by eunaumtenhoid (c) 2017 [ver 1.0.0-a1] (https://github.com/phpBBTraducoes)
+ * @copyright (c) 2016 phpBB Limited <https://www.phpbb.com>
+ * @license GNU General Public License, version 2 (GPL-2.0)
+ *
+ */
+
+/**
+ * DO NOT CHANGE
+ */
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+$lang = array_merge($lang, array(
+	'TOPIC_PREFIXES'			=> 'Prefixos do tópico',
+	'TOPIC_PREFIXES_EXPLAIN'	=> 'A partir desta página você pode gerenciar prefixos de tópicos para fóruns.',
+
+	'TOPIC_PREFIX_TAG'			=> 'Tag do prefixo',
+	'TOPIC_PREFIX_ENABLED'		=> 'Prefixo Ativado',
+	'TOPIC_PREFIXES_EMPTY'		=> 'Não existem prefixos para este fórum.',
+
+	'CREATE_TOPIC_PREFIX'			=> 'Crie um novo prefixo de tópico',
+	'DELETE_TOPIC_PREFIX_CONFIRM'	=> 'Tem certeza de que deseja deletar o prefixo do tópico?',
+	'TOPIC_PREFIX_DELETED'			=> 'O prefixo do tópico foi deletado.',
+
+	'TOPIC_PREFIX_TOGGLE_STATE'	=> array(
+		0	=> 'Clique para ativar',
+		1	=> 'Clique para desativar',
+	),
+
+	// Nested set exception messages
+	'TOPIC_PREFIXES_LOCK_FAILED_ACQUIRE'	=> 'A extensão de prefixos do tópico não conseguiu adquirir um bloqueio na tabela.',
+	'TOPIC_PREFIXES_INVALID_ITEM'			=> 'O prefixo do tópico solicitado não existe.',
+	'TOPIC_PREFIXES_INVALID_PARENT'			=> 'O prefixo do tópico solicitado não tem pai.',
+));
---
+++ language/pt_br/topic_prefixes.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ *
+ * Topic Prefixes extension for the phpBB Forum Software package.
+ * Brazilian Portuguese translation by eunaumtenhoid (c) 2017 [ver 1.0.0-a1] (https://github.com/phpBBTraducoes)
+ * @copyright (c) 2016 phpBB Limited <https://www.phpbb.com>
+ * @license GNU General Public License, version 2 (GPL-2.0)
+ *
+ */
+
+/**
+ * DO NOT CHANGE
+ */
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+$lang = array_merge($lang, array(
+	'TOPIC_PREFIX'			=> 'Prefixo do tópico',
+));
